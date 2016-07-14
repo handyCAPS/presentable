b000ecc5af149318db1308c1b7c5e509b6be1a3b
Tim Doppenberg <info@timdoppenberg.nl>
2016-07-14 10:01:20 +0200

    adds index'

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // This is it
 
+import React from 'react';
+
 const test = 'test';
 
-console.log("Test: ", test);
+console.log("Test 3 ", test);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,68 @@
+const path = require('path');
+
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
+
     entry: './src/js/index.js',
     output: {
-        path: './dist/js',
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist/js'),
+        filename: 'bundle.js',
+        publicPath: '/dist/js'
+    },
+    devServer: {
+      inline: true,
+      port: 3333,
+      historyApiFallback: true
     },
     module: {
-        loaders: [
-            {
-                test: /\.js/,
-                loader: ['babel'],
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015'],
-                    plugins: ['transform-object-rest-spread'],
-                    cacheDirectory: './.webpack-cache'
-                }
-            }
-        ]
+      loaders: [
+      // js
+      {
+        test: /\.js$/,
+        loader: ['babel'],
+        // include: path.join(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        query: {
+            presets: ['es2015', 'react'],
+            plugins: ['transform-object-rest-spread'],
+            cacheDirectory: './.webpack-cache'
+        }
+      },
+      // scss
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass'],
+        exclude: /node_modules/
+      }
+      ]
+    },
+    sassLoader: {
+      // includePaths: [ path.join(__dirname, 'src/scss')]
+    },
+    postcss: function() {
+      return [autoprefixer];
     }
+
 };
+
+// module.exports = {
+//   entry: './dev/jsx/index.js',
+//   output: {
+//     path: 'dist/js',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js?$/,
+//         exclude: /(node_modules|bower_components|backend|dashboard|dist|templates)/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react'],
+//           cacheDirectory: '.webpack-cache'
+//         }
+//       }
+//     ]
+//   }
+// };
