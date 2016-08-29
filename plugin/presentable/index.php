eb7af2ebd84e763292af6f45187481c1bbdeb83f
Tim Doppenberg <info@timdoppenberg.nl>
2016-08-29 08:41:44 +0200

    started plugin

---
+++ plugin/presentable/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Presentable
+ * Plugin URI:        https://github.com/handyCAPS/presentable
+ * Description:       Presentable let's user sign in and out
+ * Version:           0.1.0
+ * Author:            Tim Doppenberg
+ * Author URI:        http://timdoppenberg.nl
+ * Text Domain:       presentable
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /l10n
+ * GitHub Plugin URI: https://github.com/handyCAPS/presentable
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ){
+    die;
+}
